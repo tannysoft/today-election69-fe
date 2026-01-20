d0c128683eacbe224f306ac53279bd88c0d4f626
Tannysoft <tannysoft@gmail.com>
2026-01-21 00:24:53 +0700

    edit range text color

--- src/components/LowerThird/ScoreCard.js
+++ src/components/LowerThird/ScoreCard.js
@@ -19,7 +19,7 @@ export default function ScoreCard({ rank, name, party, partyLogoUrl, score, colo
         const g = parseInt(hex.slice(3, 5), 16);
         const b = parseInt(hex.slice(5, 7), 16);
         const yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
-        return (yiq >= 150) ? 'black' : 'white';
+        return (yiq >= 128) ? 'black' : 'white';
     };
 
     const textColor = isHex ? getTextColor(color) : 'white'; // Default white for preset gradients
